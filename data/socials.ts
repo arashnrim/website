6706a8d6adc371231b48a1f513e154aed5da4d63
Arash <47273556+arashnrim@users.noreply.github.com>
2023-03-22 02:50:07 +0800

    style: improve stylings
    To be fair, this is a combination of changes, and I forgot what was done for each file.

---
+++ data/socials.ts
@@ -0,0 +1,25 @@
+const socials = [
+  {
+    name: "Email",
+    description: "A general, catch-all address for anything",
+    url: "mailto:hello@arashnrim.me",
+    linkText: "hello [@] arashnrim [.] me",
+    iconName: "ph:envelope-simple",
+  },
+  {
+    name: "LinkedIn",
+    description: "For professional inquiries and networking",
+    url: "https://linkedin.com/in/arashnrim",
+    linkText: "arashnrim",
+    iconName: "simple-icons:linkedin",
+  },
+  {
+    name: "GitHub",
+    description: "Source code for my projects and contributions",
+    url: "https://github.com/arashnrim",
+    linkText: "arashnrim",
+    iconName: "simple-icons:github",
+  },
+];
+
+export default socials;
